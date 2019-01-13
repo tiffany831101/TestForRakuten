02a4fc2766330c23ec690cb73b23946cacaee3da
tiffany <tiffany831101@gmail.com>
2019-01-13 21:36:49 +0800

    Updates

---
+++ precache-manifest.929056118afb5b1fcd67c22206d5544a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "990f2a6f3c7d35f9d679",
+    "url": "/TestForRakuten/static/js/runtime~main.990f2a6f.js"
+  },
+  {
+    "revision": "088d7ecc73bf0095db79",
+    "url": "/TestForRakuten/static/js/main.088d7ecc.chunk.js"
+  },
+  {
+    "revision": "5e481933f3b1f2bcfe7e",
+    "url": "/TestForRakuten/static/js/1.5e481933.chunk.js"
+  },
+  {
+    "revision": "088d7ecc73bf0095db79",
+    "url": "/TestForRakuten/static/css/main.abc02157.chunk.css"
+  },
+  {
+    "revision": "e86193ee20757f08620b20eda8736f2f",
+    "url": "/TestForRakuten/index.html"
+  }
+];
